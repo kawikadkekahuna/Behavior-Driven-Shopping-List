406af4e93167140cf07bcc371952aef2a75c98fa
Brad Ashburn <you@example.com>
2015-06-06 17:05:45 -1000

    wrote ShoppingListItem

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -1,1 +1,15 @@
-console.log('hi');
+function ShoppingListItem(name, desc) {
+  this.name = name;
+  this.description = desc;
+  this.is_done;
+  this.check = function() {
+    this.is_done = true;
+  }
+  this.uncheck = function() {
+    this.is_done = false;
+  }
+  this.render = function() {
+    var html_string = '<ul><li class=completed_' + this.is_done + '><span>' + this.name + '</span><span>' + this.description + '</span></li></ul>';
+    return html_string;
+  }
+}
